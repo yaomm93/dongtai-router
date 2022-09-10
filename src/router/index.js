9764c5e034b8bb52c9cb31ea6eaf6922d8191472
yaomm93 <yaomm93@163.com>
2022-09-10 23:13:36 +0800

    第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+// import About from '../views/About.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,6 +16,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // component: About
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
